bff82e29f248b71b07295c810f40ed2e4bed18e3
joeil Mohali <yaj92250>
2023-03-12 16:30:37 +0100

    add roles and length of roles

--- client/src/component/Role.tsx
+++ client/src/component/Role.tsx
@@ -1,8 +1,20 @@
-import {roles} from "../screen/Game/Game";
+
 import {socketContext, ExtendedSocket} from "../context/socket";
 import { useContext } from "react";
 
-function Role({name, description, side, max}: roles) {
+interface role {
+    name: string,
+    description: string,
+    side: string,
+    max: number,
+    roleArray: string[] | undefined
+}
+
+function Role({name, description, side, max, roleArray}: role) {
+
+    const nbRole = roleArray?.filter((role) => {
+        return role === name
+    }).length
 
     const socket = useContext<ExtendedSocket>(socketContext);
 
@@ -15,11 +27,11 @@ function Role({name, description, side, max}: roles) {
     }
     
     return (
-        <div className="col-sm-2">
+        <div className="col-sm-2 text-center">
             <p data-bs-toggle="tooltip" data-bs-placement="top" title={description}>
-            {name}
+            {name} ({nbRole})
             </p>
-            <div className="d-flex">
+            <div className="">
                 <button className="btn btn-danger mx-1" onClick={() => {deleteRole(name)}}>-</button>
                 <button className="btn btn-success mx-1" onClick={() => {addRole(name)}}>+</button>            
             </div>
--- client/src/screen/Game/Game.tsx
+++ client/src/screen/Game/Game.tsx
@@ -15,12 +15,25 @@ export interface roles {
     max: number
 }
 
+export interface room {
+    status?: string,
+    author?: string,
+    players?: string[],
+    roles?: string[],
+    votes?: string[]
+}
+
 function Game() {
 
     const { id } = useParams<Params>();
     const [roles, setRoles] = useState<roles[] | null>(null);
+    const [room, setRoom] = useState<room | null>(null);
     const socket = useContext<ExtendedSocket>(socketContext);
 
+    socket.on('getRoom', room => {
+        setRoom(room);
+        console.log(room);
+    })
     
     socket.on('getRoles', (roles) => {
         setRoles(roles);
@@ -30,10 +43,12 @@ function Game() {
     useEffect(() => {
         if (!roles) {
             socket.emit('getRoles');
-            console.log("coucou");
-            
         }
-    }, [socket, roles])
+
+        if (!room) {
+            socket.emit('getRoom');
+        }
+    }, [socket, roles, room])
 
     return (
         <div className='container-fluid'>
@@ -41,12 +56,24 @@ function Game() {
                 <div className="col-md-2 d-none d-md-block bg-light sidebar">
                 </div>
                 <div className="col">
-                    <div className="row my-5 container m-auto">
-                        <h2 className='text-center my-3'>Choissisez les rôles</h2>
-                        {roles?.map((role: roles, index: number) => (
-                            <Role name={role.name} description={role.description} side={role.side} max={role.max} key={index} />
-                        ))}
+                    <div className="card bg-light container m-auto my-5">
+                        <div className="card-body text-center">
+                            <div className="row my-5">
+                                <h2 className='my-3'>Choissisez les rôles</h2>
+                                {roles?.map((role: roles, index: number) => (
+                                    <Role 
+                                    name={role.name} 
+                                    description={role.description} 
+                                    side={role.side} 
+                                    max={role.max}
+                                    roleArray={room?.roles}
+                                    key={index} />
+                                ))}
+                            </div>
+                            <button className="btn btn-success btn-lg">Lancer la partie</button>
+                        </div>
                     </div>
+                    <div className="card container bg-light p-3 text-center">Identifiant : {id}</div>
                 </div>
             </div>
         </div>
